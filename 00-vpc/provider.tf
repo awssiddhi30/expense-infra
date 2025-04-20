2a48452577fee30675a757655bacfed2001ab7b4
awssiddhi30 <psd.shardadevi30@gmail.com>
2025-04-20 12:34:50 +0530

    new file

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -9,7 +9,7 @@ terraform {
  
   backend "s3" {
     bucket = "daws30-s3-dev"
-    key    = "expense-vpc" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    key    = "expense-sg" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "dynamodb-tf-remote-dev"
   }
