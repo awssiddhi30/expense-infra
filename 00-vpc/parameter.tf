c49155d158463223056764d1414905e232da2a18
awssiddhi30 <psd.shardadevi30@gmail.com>
2025-04-20 17:08:39 +0530

    new file

--- 00-vpc/parameter.tf
+++ 00-vpc/parameter.tf
@@ -5,18 +5,21 @@ resource "aws_ssm_parameter" "vpc_id" {
   name  = "/${var.project_name}/${var.environment}/vpc_id"
   type  = "String"
   value = module.vpc.vpc_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/public_subnet_ids"
   type = "StringList"
   value = join(",", module.vpc.public_subnet_ids)
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/private_subnet_ids"
   type = "StringList"
   value = join(",", module.vpc.private_subnet_ids)
+  overwrite = true
   
 }
 
@@ -24,6 +27,7 @@ resource "aws_ssm_parameter" "database_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/database_subnet_ids"
   type = "StringList"
   value = join(",", module.vpc.database_subnet_ids)
+  overwrite = true
   
 }
 
@@ -31,4 +35,5 @@ resource "aws_ssm_parameter" "database_subnet_group_name" {
   name  = "/${var.project_name}/${var.environment}/database_subnet_group_name"
   type  = "String"
   value = module.vpc.db_subnet_group
+  overwrite = true
 }
